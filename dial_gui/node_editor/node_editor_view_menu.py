33d90043b4a8b5404f7a2c955ca636cd9d29b663
David Afonso <davafons@gmail.com>
2020-04-01 02:04:56 +0100

    :sparkles: Adding option to the menu for adding each node to each window

--- dial_gui/node_editor/node_editor_view_menu.py
+++ dial_gui/node_editor/node_editor_view_menu.py
@@ -32,7 +32,14 @@ class NodeEditorViewMenu(QMenu):
 
         self._add_nodes_to_new_window_act = QAction("Add nodes to new window", self)
         self._add_nodes_to_new_window_act.triggered.connect(
-            self.__add_nodes_to_new_window
+            self.__add_selected_nodes_to_new_window
+        )
+
+        self._add_each_node_to_new_window_act = QAction(
+            "Add each node to a new window", self
+        )
+        self._add_each_node_to_new_window_act.triggered.connect(
+            self.__add_each_selected_node_to_new_window
         )
 
         self._add_nodes_to_existing_window_menu = QMenu(
@@ -45,14 +52,15 @@ class NodeEditorViewMenu(QMenu):
         for window in self.__nodes_windows_manager.nodes_windows:
             action = self._add_nodes_to_existing_window_menu.addAction(window.name)
             action.triggered.connect(
-                lambda _=None, window=window: self.__add_nodes_to_existing_window(
-                    window
+                lambda _=None, window=window: (
+                    self.__add_selected_nodes_to_existing_window(window)
                 )
             )
 
         self.addAction(self._remove_elements_act)
         self.addSeparator()
         self.addAction(self._add_nodes_to_new_window_act)
+        self.addAction(self._add_each_node_to_new_window_act)
         self.addMenu(self._add_nodes_to_existing_window_menu)
 
     def mouseReleaseEvent(self, event):
@@ -62,22 +70,34 @@ class NodeEditorViewMenu(QMenu):
         super().mouseReleaseEvent(event)
 
     def __remove_selected_elements(self):
+        """Remove the selected elements from the scene."""
         for selected_item in self.__graphics_scene.selectedItems():
             self.__graphics_scene.removeItem(selected_item)
 
         self.__graphics_scene.update()
 
-    def __add_nodes_to_new_window(self):
+    def __add_selected_nodes_to_new_window(self):
+        """Add the selected nodes from a new NodesWindow object."""
         nodes_window = self.__nodes_windows_manager.new_nodes_window()
+        self.__add_nodes_to_window(self.__graphics_scene.selectedItems(), nodes_window)
 
-        for selected_item in self.__graphics_scene.selectedItems():
-            if isinstance(selected_item, GraphicsNode):
-                nodes_window.add_graphics_node(selected_item)
+    def __add_selected_nodes_to_existing_window(self, window: "NodesWindow"):
+        """Add the selected nodes to a currently existing NodesWindow object."""
+        self.__add_nodes_to_window(self.__graphics_scene.selectedItems(), window)
 
-    def __add_nodes_to_existing_window(self, window: "NodesWindow"):
-        for selected_item in self.__graphics_scene.selectedItems():
-            if isinstance(selected_item, GraphicsNode):
-                window.add_graphics_node(selected_item)
+    def __add_each_selected_node_to_new_window(self):
+        """For each selected node, add it to a new window."""
+        for graphics_node in self.__graphics_scene.selectedItems():
+            nodes_window = self.__nodes_windows_manager.new_nodes_window(
+                name=graphics_node.title
+            )
+            self.__add_nodes_to_window([graphics_node], nodes_window)
+
+    def __add_nodes_to_window(self, graphics_nodes, window):
+        """Add the nodes to the passed window."""
+        for graphics_node in graphics_nodes:
+            if isinstance(graphics_node, GraphicsNode):
+                window.add_graphics_node(graphics_node)
 
 
 NodeEditorViewMenuFactory = providers.Factory(NodeEditorViewMenu)
